fc9c2425da9be1c7a064e70263c06591f05a6cbb
faiztest <129721483+faiztest@users.noreply.github.com>
2023-04-03 13:27:53 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -104,5 +104,5 @@ if uploaded_file is not None:
         topics, probs = topic_model.fit_transform(topic_abs)
         freq = topic_model.get_topic_info(); freq.head(5)
         topic_model.get_topic(0)
-        topic_model.topic_representations_(6)
+        #topic_model.topic_representations_(6)
         topic_model.visualize_topics()
