bcebfd041cad6f41f5c9534dcab0c830bb76abb1
faiztest <129721483+faiztest@users.noreply.github.com>
2023-05-09 15:49:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -112,12 +112,13 @@ if uploaded_file is not None:
         num_btopic = st.slider('Choose number of topics', min_value=4, max_value=50, step=1)
         topic_abs = paper.Abstract_stop.values.tolist()
         topic_time = paper.Year.values.tolist()
-        topic_model = BERTopic()
+        topic_model = BERTopic(verbose=True, embedding_model="paraphrase-MiniLM-L12-v2", min_topic_size=50)
+        topics, _ = topic_model.fit_transform(abstracts); len(topic_model.get_topic_info())
         #hdbscan_model = HDBSCAN(min_cluster_size=num_btopic, metric='euclidean', cluster_selection_method='eom', prediction_data=True)
         #topic_model = BERTopic(hdbscan_model=hdbscan_model).fit(topic_abs)
         #cluster_model = KMeans(n_clusters=num_btopic)
         #topic_model = BERTopic(hdbscan_model=cluster_model).fit(topic_abs)
-        topics, probs = topic_model.fit_transform(topic_abs)
+        #topics, probs = topic_model.fit_transform(topic_abs)
         
         tab1, tab2 = st.tabs(["📈 Generate visualization", "Reference"])
         with tab1:
