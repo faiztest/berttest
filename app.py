2b5c520004ec2f55a6a1c6aa5b4f2cd3afb11627
faiztest <129721483+faiztest@users.noreply.github.com>
2023-08-09 11:22:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -198,7 +198,7 @@ if uploaded_file is not None:
                             nodes.append( Node(id=x, 
                                             label=x,
                                             size=50*w+10,
-                                            shape="ellipse",
+                                            shape="dot",
                                             labelHighlightBold=True,
                                             group=x,
                                             opacity=10,
